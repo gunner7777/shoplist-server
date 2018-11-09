ef0c88683fdb7aefe8e58089b1b061995bd618e3
gunner7777 <123@yandex.ru>
2018-11-09 16:31:36 +0300

    dont show old list

--- src/modules/shopping/shopping.controller.js
+++ src/modules/shopping/shopping.controller.js
@@ -4,6 +4,10 @@ import moment from 'moment';
 export async function showCurrent(req, res) {
   try {
     let shopping = await Shopping.findOne({}).sort({ created_at: -1 }).populate('products');
+
+    if(moment().diff(moment(shopping.created_at), 'days') > 4)
+      shopping = {};
+
     return res.status(201).json(shopping);
   } catch (err) {
     return res.status(500).json(err);
@@ -15,9 +19,11 @@ export async function addShopping(req, res) {
     let saveData = await Shopping.create({
       //created_at: moment().format('DD.MM.YYYY'),
       name: req.body.name,
-      products: ['5b88ef6b19bd9f21c8340bb3', '5b855362ff10362ce8269816'],
+      products: req.body.products,
     });
-    return res.status(201).json('shopping list has been added');
+    let fullInfo = await saveData.populate('products').execPopulate();
+    //return res.status(201).json('shopping list has been added');
+    return res.status(201).json(fullInfo);
   } catch (err) {
     return res.status(500).json(err);
   }
@@ -27,8 +33,8 @@ export async function deleteShopping(req, res) {
   try {
     //let delList = await Shopping.deleteOne({_id: req.params.id});
     let delList = await Shopping.findOneAndDelete({_id: req.params.id});
-    //res.json(delList);
-    res.status(201).json(`Shopping list with id ${req.params.id} has been deleted`);
+    res.status(201).json(delList);
+    //res.status(201).json(`Shopping list with id ${req.params.id} has been deleted`);
   } catch (err) {
     return res.status(500).json(err);
   }
@@ -37,10 +43,12 @@ export async function deleteShopping(req, res) {
 export async function updateShopping(req, res) {
   try {
     const editInfo = {
-      products: req.body.products
+      products: req.body.products,
+      name: req.body.name,
     };
-
-    let shopping = await Shopping.findOneAndUpdate({ _id: req.params.id }, editInfo);
+    const options = { new: true };
+    //let shopping = await Shopping.findOneAndUpdate({ _id: req.params.id }, editInfo, options);
+    let shopping = await Shopping.findOneAndUpdate({ _id: req.params.id }, {$set: req.body}, options).populate('products');
     res.json(shopping);
     //let ff = await shopping.findOne({ _id: req.params.pid });
     //res.json(ff);
